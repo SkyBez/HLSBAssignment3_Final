774101c89ed1ee92b808c1dcd6d511bf07148636
SkyBez <124073801+SkyBez@users.noreply.github.com>
2023-03-28 17:48:16 +0000

    hi

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express = require("express");
+const ejs = require("ejs");
 const path = require("path");
 const app = express();
 
+//sets our view engine to load files ending in .ejs
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const PORT = 3000;
+const PORT = 4000;
 
 app.listen(PORT, () => {
   console.log("App listening on port ", PORT);
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/", "index.html"));
+  //res.sendFile(path.resolve(__dirname + "/views/", "index.html"));
+  res.render("index");
 });
 
 app.get("/profile", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/", "profile.html"));
+  //res.sendFile(path.resolve(__dirname + "/views/", "profile.html"));
+  res.render("profile");
 });
 
 app.get("/math", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/", "math.html"));
+  //res.sendFile(path.resolve(__dirname + "/views/", "math.html"));
+  res.render("math");
 });
 
 app.get("/faq", (req, res) => {
@@ -29,3 +35,8 @@ app.get("/faq", (req, res) => {
 app.get("/terms", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/views/", "terms.html"));
 });
+
+app.get("/slides", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/views/", "slide-show.html"));
+});
+
--- public/js/slide-show.js
+++ public/js/slide-show.js
@@ -2,6 +2,43 @@
 
 const $ = (selector) => document.querySelector(selector);
 
+let imageCounter = 0;
+const caption = $("#caption");
+const mainImage = $("#main_image");
+let imageCache = [];
+
+const swapImage = ()=>{
+    imageCounter = (imageCounter+1) % (imageCache.length-1);
+    
+    mainImage.src = imageCache[imageCounter].src;
+    mainImage.alt = imageCache[imageCounter].alt;
+
+    caption.textContent = imageCache[imageCounter].alt;
+
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
+
+
+    const links= document.querySelectorAll("a");
+
+    let image;
+
+
+
+    for(let link of links){
+        image = new Image();
+
+        image.src = link.href;
+        image.alt = link.title;
+
+        imageCache.push(image);
+
+
+    }
+
+    setInterval(swapImage, 2000);
+
 
 });
